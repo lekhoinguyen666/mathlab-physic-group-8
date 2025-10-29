00e1ece89ed68293818219b1ad7662eac6dbd2a2
Le Khoi Nguyen <155555943+lekhoinguyen666@users.noreply.github.com>
2025-10-29 13:32:14 +0700

    Update index.m

--- index.m
+++ index.m
@@ -1,10 +1,5 @@
-% Task 1 (Nguyen)
-
-
-% Task 2 (Nhan)
-
-
-% Task 3 (Nam)
-
-
-% Task 4
+% "Importing files" by execute them which will create usable variables in MATLAB workspace tab
+task-1
+task-2
+task-3
+task-4
