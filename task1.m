d37658b9bb669dcb78e2e333308de22b53e408ef
lekhoinguyen06 <khoinguyenbenz@gmail.com>
2025-11-26 02:09:26 +0000

    Nhan done task 3, modified file name error

--- task1.m
+++ task1.m
@@ -11,7 +11,7 @@ disp('Task 1');
 
 % Ask user to input potential expression
 disp('Enter the electric potential V(x, y):');
-disp('Example: x^2 + y^2   or   sin(x)*cos(y)');
+disp('Example: x.^2 + y.^2   or   sin(x)*cos(y)');
 V_input = input('V(x,y) = ', 's');   % Get user input as string
 
 % Convert input string to math expression
